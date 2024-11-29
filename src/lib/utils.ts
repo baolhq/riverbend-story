0f1c02ab518c404cd68241e5f8ce831cd98e2f1f
baolhq <baolhq280@gmail.com>
2024-11-29 14:35:05 +0700

    added shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-export function formatDate(input: string | number): string {
-  const date = new Date(input);
-  return date.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
